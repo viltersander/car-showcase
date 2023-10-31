ebdc6226db0675d14b0ee3998c1bc80fa41ed2b8
viltersander <sander.vilter123@gmail.com>
2023-10-31 15:20:12 +0200

    footer copyright display changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,8 +32,8 @@ const Footer = () => {
         </div>
         </div>          
         
-        <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-          <p>@2023 CarHub. All Rights Reserved</p>
+        <div className="flex sm:justify-between flex-col-reverse sm:flex-row items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+          <p className="pt-4 sm:pt-0">@2023 CarHub. All Rights Reserved</p>
           <div className="footer__copyrights-link">
             <Link href="/" className="text-gray-500">
               Privacy Policy
